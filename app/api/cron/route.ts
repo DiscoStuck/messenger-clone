90df16bdb58b03e8e8a51cf19adc68e81b32dccb
Disco <jjotasaimon@gmail.com>
2023-08-08 15:52:00 +0700

    Reset script export default added v1.1

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 const prisma = new PrismaClient();
 
-export default async function resetDb(req: NextApiRequest, res: NextApiResponse) {
+async function resetDb(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     // Delete all data from all tables
     await prisma.user.deleteMany();
@@ -50,3 +50,5 @@ export default async function resetDb(req: NextApiRequest, res: NextApiResponse)
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
+
+export default resetDb
