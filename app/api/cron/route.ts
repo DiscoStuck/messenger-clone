4f63f8c2d3a0a3c4e1e11c3832ef0499d5da29c4
Disco <jjotasaimon@gmail.com>
2023-08-09 23:00:08 +0700

    changes in script

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -3,18 +3,18 @@ import { PrismaClient } from '@prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
-    const prisma = new PrismaClient();
-const createdUsers = [];
-    try {
-        // Delete all data from all tables
-        await prisma.user.deleteMany();
-        await prisma.account.deleteMany();
-        await prisma.conversation.deleteMany();
-        await prisma.message.deleteMany();
+  const prisma = new PrismaClient();
+  const createdUsers = [];
+  try {
+    // Delete all data from all tables
+    const deletedUsers = await prisma.user.deleteMany();
+    const deletedAccounts = await prisma.account.deleteMany();
+    const deletedConversations = await prisma.conversation.deleteMany();
+    const deletedMessages = await prisma.message.deleteMany();
 
-        // Create users
-        const users = [
-                  {
+    // Create users
+    const users = [
+      {
         name: 'John',
         email: 'john@gmail.com',
         image: 'https://res.cloudinary.com/dqunjerlj/image/upload/v1691482972/pexels-photo-220453_hv5shk.webp',
@@ -29,23 +29,34 @@ const createdUsers = [];
         email: 'lydia@gmail.com',
         image: 'https://res.cloudinary.com/dqunjerlj/image/upload/v1691482972/pexels-photo-415829_qaivyk.webp',
       },
-        ];
+    ];
 
         
-        for (const user of users) {
-            const createdUser = await prisma.user.create({
-                data: {
-                    name: user.name,
-                    email: user.email,
-                    image: user.image,
-                },
-            });
-            createdUsers.push(createdUser);
-        }
-    } finally {
-        // Disconnect Prisma Client
-        await prisma.$disconnect();
+    for (const user of users) {
+      const createdUser = await prisma.user.create({
+        data: {
+          name: user.name,
+          email: user.email,
+          image: user.image,
+        },
+      });
+      createdUsers.push(createdUser);
     }
+    await prisma.$disconnect();
+  
+    return NextResponse.json({
+      message: 'Database reset successful',
+      dataDeleted: {
+        users: deletedUsers.count,
+        accounts: deletedAccounts.count,
+        conversations: deletedConversations.count,
+        messages: deletedMessages.count
+      },
+      dataSaved: createdUsers
+    }, { status: 200 });
 
-    return NextResponse.json( { message: 'Database reset successful', dataSaved: createdUsers }, { status: 200 } );
+  }  catch (error) {
+        console.error(error);
+        return NextResponse.json( { message: 'Database reset failed' }, { status: 500 } );
+    }
 }
